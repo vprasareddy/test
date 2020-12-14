fd42f8085d4c1d69a29ba33ba91279045318d158
vprasareddy <71990141+vprasareddy@users.noreply.github.com>
2020-12-14 11:12:00 -0500

    Add files via upload

---
+++ App.java
@@ -0,0 +1,21 @@
+package com.fiexch.orderbook;
+
+import java.util.List;
+
+public class App {
+	
+	public static void main(String[] args) {
+		System.out.println("Start App");
+		//Read CSV file 
+		OrderProcessor orderProcessor = new OrderProcessor();
+		List<Order> orders = orderProcessor.readLinesFromCSV();
+		if(orders != null) {
+			orderProcessor.processOrders(orders);
+			orderProcessor.createFiles(orders);
+		}
+		//Debug Info
+		for(Order order : orders) {
+			System.out.println(order.toString());
+		}
+	}
+}
